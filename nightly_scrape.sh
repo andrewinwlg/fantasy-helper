f193328059784d3e76bf2c3d6a77efed69b1cb1d
andrewinwlg <andrew.l.cohen@gmail.com>
2025-02-06 08:59:24 +1300

    Add team roster update  and error checks to nightly scrape script

--- nightly_scrape.sh
+++ nightly_scrape.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -u
 
+if [ -z "$NBA_LOGIN" ] || [ -z "$NBA_PWD" ]; then
+    echo "Error: NBA_LOGIN and NBA_PWD environment variables must be set"
+    exit 1
+fi
+
 # Ensure the /tmp/logs directory exists
 mkdir -p /tmp/logs
 
@@ -11,10 +16,23 @@ exec > >(stdbuf -oL tee -a "$LOG_FILE") 2>&1
 
 echo "NBA scraper script started at $(date)"
 
+echo "Scraping team roster"
+python3 team_scraper.py
+
+echo "Updating team roster file"
+python3 utils.py --update $NBA_LOGIN
+
 cd /mnt/c/BUILD/git_build/fantasy-helper
 python3 incremental_update.py
 python3 salary_scraper.py
+
 python3 optimize_roster.py --salary-cap 101.3 --transactions 2 --exclude "Keaton Wallace"
 python3 utils.py --print-team
+
+if grep -q "error" "$LOG_FILE"; then
+    echo "Check the log file for error messages."
+else
+    echo "No errors found."
+fi
 
 echo "NBA scraper script finished at $(date)"
